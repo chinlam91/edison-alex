19519f9ea1609535f571c9a2fce5d69a41e5bff3
Alexander <nikolas.l.alexander@gmail.com>
2017-04-19 10:32:20 +0800

    Initiate Repo

---
+++ index.js
@@ -0,0 +1,39 @@
+let express = require('express')
+let app = express()
+let webdriver = require('selenium-webdriver')
+let By = webdriver.By
+let until = webdriver.until
+
+let credentials = {
+  username: 'arkyria',
+  password: new Buffer('ZGFya3NsYXllcg==', 'base64').toString('ascii')
+}
+
+let merchants = ['arkyria']
+
+let driver = new webdriver.Builder().forBrowser('firefox').build()
+function login() {
+  driver.get('http://www.instagram.com/')
+  let loginButton = driver.wait(until.elementLocated(By.css('._fcn8k')), 10000)
+  loginButton.click()
+  let usernameInput = driver.wait(until.elementLocated(By.name('username')), 2000)
+  usernameInput.sendKeys(credentials.username)
+  driver.findElement(By.name('password')).sendKeys(credentials.password)
+  driver.findElement(By.css('._ah57t._84y62')).click()
+  driver.wait(until.elementLocated(By.css('._9x5sw')), 10000)
+}
+
+function scanMerchants() {
+  for (let i in merchants) {
+    let merchant = merchants[i]
+    driver.get('http://www.instagram.com/'+merchant)
+    driver.findElement(By.css('a._s53mj[href="/'+merchant+'/followers/"]')).click()
+    let dialog = driver.findElement(By.css('._4gt3b'))
+    driver.executeScript(function() {
+      document.getElementsByClassName('_4gt3b')[0].scrollTo(0, document.getElementsByClassName('_4gt3b')[0].scrollHeight)
+    })
+  }
+}
+
+login()
+scanMerchants()
