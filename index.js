131a03cf44f56f15ae2752274aac7d27943272c0
Alexander <nikolas.l.alexander@gmail.com>
2017-05-12 17:44:08 +0800

    Added scanfollowers function

--- index.js
+++ index.js
@@ -22,16 +22,29 @@ function login() {
   driver.findElement(By.css('._ah57t._84y62')).click()
   driver.wait(until.elementLocated(By.css('._9x5sw')), 10000)
 }
+let counter = 5
+function scanFollowers() {
+  
+  driver.executeScript(function() {
+    let prevHeight = document.getElementsByClassName('_4gt3b')[0].scrollHeight
+    setTimeout(function() {
+      document.getElementsByClassName('_4gt3b')[0].scrollTo(0, document.getElementsByClassName('_4gt3b')[0].scrollHeight)
+      let nextHeight = document.getElementsByClassName('_4gt3b')[0].scrollHeight
+      if (prevHeight < nextHeight && counter) {
+        counter--
+        scanFollowers()
+      }
+    }, 500)
+  })
+}
 
 function scanMerchants() {
   for (let i in merchants) {
     let merchant = merchants[i]
     driver.get('http://www.instagram.com/'+merchant)
     driver.findElement(By.css('a._s53mj[href="/'+merchant+'/followers/"]')).click()
-    let dialog = driver.findElement(By.css('._4gt3b'))
-    driver.executeScript(function() {
-      document.getElementsByClassName('_4gt3b')[0].scrollTo(0, document.getElementsByClassName('_4gt3b')[0].scrollHeight)
-    })
+    let dialog = driver.wait(until.elementLocated(By.css('._cx1ua')), 10000)
+    scanFollowers()
   }
 }
 
